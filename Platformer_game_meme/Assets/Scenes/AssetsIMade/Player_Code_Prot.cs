a0f780360f033c381cd8b2d8f73b3daa45f440d6
Saladsnek <505188@chrome.escg.ac.uk>
2019-10-22 16:28:41 +0100

    Sprites, temporary and WiP commited. Plus code for left and right movement done.

---
+++ Platformer_game_meme/Assets/Scenes/AssetsIMade/Player_Code_Prot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Code_Prot : MonoBehaviour
+{
+    public int playerSpeed = 10;
+    public bool facingRight = true;
+    public int playerJumpPower = 1250;
+    public float moveX;
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        //CONTROLS
+        moveX = Input.GetAxis("Horizontal");
+        //ANIMATION
+        //PLAYER DIRECTION
+        if (moveX < 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
+        else if (moveX > 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+        //PHYSICS
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+    void Jump()
+    {
+        //JUMPING CODE
+    }
+    void FlipPlayer()
+    {
+
+    }
+}
