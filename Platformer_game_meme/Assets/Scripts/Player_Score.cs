67c881bbed2e2869e21bcb1f046da11c8dba143e
Saladsnek <505188@chrome.escg.ac.uk>
2019-11-11 16:21:24 +0000

    Scoring and time on screen

--- Platformer_game_meme/Assets/Scripts/Player_Score.cs
+++ Platformer_game_meme/Assets/Scripts/Player_Score.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player_Score : MonoBehaviour
 {
 
     private float timeLeft = 120;
     public int playerScore = 0;
+    public GameObject timeLeftUI;
+    public GameObject playerScoreUI;
 
     // Update is called once per frame
     void Update()
     {
         timeLeft -= Time.deltaTime;
+        timeLeftUI.gameObject.GetComponent<Text>().text = ("Time Left : " + (int)timeLeft);
+        playerScoreUI.gameObject.GetComponent<Text>().text = ("Score : " + playerScore);
         if (timeLeft < 0.1f)
         {
             SceneManager.LoadScene("Prototype_Scene");
@@ -20,6 +25,11 @@ public class Player_Score : MonoBehaviour
     }
     void OnTriggerEnter2D (Collider2D trig)
     {
-        Debug.Log("Touched End");
+        CountScore();
+    }
+    void CountScore()
+    {
+        playerScore = playerScore + ((int)timeLeft * 5);
+        Debug.Log(playerScore);
     }
 }
