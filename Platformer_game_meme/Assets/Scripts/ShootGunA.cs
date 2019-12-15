c38ddbcdc799358a379dabb03dc2f0900db92ea5
Saladsnek <505188@chrome.escg.ac.uk>
2019-12-15 20:55:24 +0000

    Began next character, Ashen, uploaded some gun SFX.

---
+++ Platformer_game_meme/Assets/Scripts/ShootGunA.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootGunA : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
